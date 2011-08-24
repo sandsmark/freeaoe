056f1562a3232da8fa94450f0ba579d283afdd39
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-25 01:39:23 +0200

    main.cpp: update to recent GIT version of SFML
    	- Line 57: now use sf::RenderWindow App.PollEvent() instead of sf::RenderWindow App.GetEvent()

--- main.cpp
+++ main.cpp
@@ -20,46 +20,46 @@
 // The main function behaves now as a testbed, real code should not be written
 // here.
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
   Logger::setLogLevel(Logger::L_INFO);
-  
+
   DatFile df;
   df.open("aoe2/Data/empires2.dat");
-  
+
   //Engine en;
   //en.start();
-  
+
   ResourceManager::Inst();
   DataManager::Inst();
   sf::Clock clk;
- 
+
   GenieGraphic gg = DataManager::Inst()->getGraphic(1972);//1128);
   RenderGraphic rg(&gg);
   rg.setX(100);
   rg.setY(150);
-  
+
   Map map;
   map.setUpSample();
-  
+
   sf::RenderWindow App(sf::VideoMode(800, 600), "SFML window");
   sf::View view(sf::FloatRect(0,0,300,300));
-  
+
   MapRenderer render(&App);
   render.setMap(&map);
-  
+
   bool pause = false;
    // Start the game loop
      while (App.IsOpened())
      {
          // Process events
          sf::Event Event;
-         while (App.GetEvent(Event))
+         while (App.PollEvent(Event))
          {
              // Close window : exit
              if (Event.Type == sf::Event::Closed)
                  App.Close();
-             
+
              if (Event.Type == sf::Event::KeyReleased)
              {
                 if (Event.Key.Code  == sf::Key::Space)
@@ -71,22 +71,22 @@ int main(int argc, char **argv)
         {
          // Clear screen
          App.Clear();
- 
+
          //App.SetView(view);
          // Draw the sprite
          render.Draw();
-         
+
          rg.drawOn(&App);
-         
+
          App.SetView(App.GetDefaultView());
- 
+
          // Draw the string
- 
+
          // Update the window
          App.Display();
         }
-         
+
      }
-     
+
   return 0;
 }
