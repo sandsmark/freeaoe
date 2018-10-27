8f4c32da2b7c13e0e7a2d9c7fbbf7461fd01a33b
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2018-10-28 00:31:32 +0200

    add some comments

--- src/resource/Terrain.cpp
+++ src/resource/Terrain.cpp
@@ -148,6 +148,8 @@ uint32_t Terrain::coordinatesToFrame(int x, int y)
 
 const sf::Texture &Terrain::texture(const MapTile &tile)
 {
+    // The original graphics code in aoe was apparently hand-written assembly according to people on the internet,
+    // and since I'm too lazy and too dumb to optimize this properly we just cache heavily instead
     std::unordered_map<MapTile, sf::Texture>::const_iterator it = m_textures.find(tile);
     if (it != m_textures.end()) {
         return it->second;
@@ -155,8 +157,15 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
 
     std::vector<uint8_t> data = m_slp->fileData();
 
+    // This defines lightning textures (e. g. to darken edges)
+    const genie::PatternMasksFile &patternmasksFile = AssetManager::Inst()->patternmasksFile();
+
+    // We need to keep operating on indexed colors with the right palette until we've done all
+    // the transformations and mapping and lighting
     const std::vector<genie::Color> colors = AssetManager::Inst()->getPalette().getColors();
-    const genie::IcmFile::InverseColorMap &icm = AssetManager::Inst()->patternmasksFile().icmFile.maps[9]; // fixme this shit
+
+    // This maps from RGB to indices in the palette
+    const genie::IcmFile::InverseColorMap &icm = patternmasksFile.icmFile.maps[genie::IcmFile::AokNeutral];
 
     uint8_t widths[49];
     uint8_t size = 1;
@@ -166,9 +175,12 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
         size += 4;
     }
 
+    // We first need to blend it and "rewrite" the image data/commands, because the texture filtering works
+    // by addressing the original commands in the image
     for (const Blend &tileBlend : tile.blends) {
         const genie::BlendMode &blendMode = AssetManager::Inst()->getBlendmode(tileBlend.blendMode);
 
+        // First generate an alpha mask that we use to blend the two frames below
         std::vector<uint8_t> alphamask(blendMode.pixelCount, 128);
         for (unsigned i=0; i < Blend::BlendTileCount; i++) {
             if ((tileBlend.bits & (1u << i)) == 0) {
@@ -189,6 +201,8 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
             int srcOffset = m_slp->frameCommandsOffset(tile.frame, y);
             int blendOffset = blendTerrain->m_slp->frameCommandsOffset(tileBlend.frame, y);
 
+            // We can assume that there aren't any fancy commands here (no player colors or shadows),
+            // so we only check the width to see if it is a lesser or greater block copy
             if (widths[y] <= 63) {
                 data[srcOffset++] = widths[y] << 2;
                 blendOffset++;
@@ -199,7 +213,7 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
             }
 
             for (int x=0; x<widths[y]; x++) {
-                uint8_t alpha = alphamask[alphaOffset++];
+                uint8_t alpha = alphamask[alphaOffset];
 
                 // Alpha goes from 0-128
                 if (alpha == 0) {
@@ -210,20 +224,29 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
                     int r = col1.r * alpha + col2.r * (128 - alpha);
                     int g = col1.g * alpha + col2.g * (128 - alpha);
                     int b = col1.b * alpha + col2.b * (128 - alpha);
+
+                    // The top five bits are used to look up the palette index
+                    // E. g. we have max 255 * 128 = 32640, if we shift that 10
+                    // we get 31 (there are 32 values for r, g and b in the ICMs)
                     data[srcOffset] = icm.paletteIndex(r >> 10, g >> 10, b >> 10);
                 }
+
                 srcOffset++;
                 blendOffset++;
+                alphaOffset++;
             }
 
             data[srcOffset++] = genie::SlpFrame::EndOfRow;
         }
     }
 
+    // This is used to alter the source SLP (for sloping), but we mostly ignore it,
+    // we only need the modified offset to the left and then use the texture filtering for the rest
     const genie::SlpTemplateFile::SlpTemplate &slpTemplate = AssetManager::Inst()->getSlpTemplateFile()->templates[tile.slopes.self.toGenie()];
+
+    // This defines the texture mapping for slopes (what pixels from the original SLP should go where)
     const genie::FiltermapFile &filterFile = AssetManager::Inst()->filtermapFile();
     const genie::FiltermapFile::Filtermap &filter = filterFile.maps[tile.slopes.self.toGenie()];
-    const genie::PatternMasksFile &patternmasksFile = AssetManager::Inst()->patternmasksFile();
 
     sf::Image image;
     image.create(m_slp->frameWidth(tile.frame), filter.height, sf::Color::Transparent);
@@ -236,12 +259,9 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
         const genie::FiltermapFile::FilterLine &line = filter.lines[y];
 
         for (uint32_t x=0; x<line.width; x++, xPos++) {
-            if (x >= line.commands.size()) {
-                WARN << "out of bounds: "  << x << " " << line.commands.size();
-            }
-
             const genie::FiltermapFile::FilterCmd &cmd = line.commands[x];
 
+            // Each target pixel can blend several source pixels
             int r = 0, g = 0, b = 0;
             for (const genie::FiltermapFile::SourcePixel &source : cmd.sourcePixels) {
                 const uint8_t sourcePaletteIndex = rawData[source.sourceIndex];
@@ -251,9 +271,13 @@ const sf::Texture &Terrain::texture(const MapTile &tile)
                 b += sourceColor.b * source.alpha;
             }
 
+            // Get the appropriate lightning from the pattern masks file
+            // There are several lightning textures used for each tile, so here we blend them
+            // together to get the inverse color map with the appropriate darkness for this pixel
             const genie::IcmFile::InverseColorMap &icm = patternmasksFile.getIcm(cmd.lightIndex, slopePatterns);
-            const int  pixelIndex = icm.paletteIndex(r >> 11, g >> 11, b >> 11);
+            const int pixelIndex = icm.paletteIndex(r >> 11, g >> 11, b >> 11);
 
+            // And then finally we get the color for a single pixel
             const genie::Color &newColor = colors[pixelIndex];
             image.setPixel(xPos, y, sf::Color(newColor.r, newColor.g, newColor.b));
         }
