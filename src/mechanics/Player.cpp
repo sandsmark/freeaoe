d85269de817cc5fe879e1ca0ff9681d6cc33eb79
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2018-12-11 15:23:30 +0100

    player: set player name by default

--- src/mechanics/Player.cpp
+++ src/mechanics/Player.cpp
@@ -5,7 +5,8 @@
 Player::Player(const int id, const std::shared_ptr<Civilization> &c, const ResourceMap &startingResources) :
     playerId(id),
     civ(c),
-    resources(civ->startingResources())
+    resources(civ->startingResources()),
+    name("Player " + std::to_string(id))
 {
     for (const std::pair<const genie::ResourceType, float> &r : startingResources) {
         resources[r.first] = r.second;
