17390101582387d785c52683259bc71a0b506da9
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-11-03 20:17:34 +0100

    fix msvc shit

--- src/ui/FileDialog.cpp
+++ src/ui/FileDialog.cpp
@@ -366,7 +366,7 @@ void ListView::setCurrentPath(std::string pathString)
 
         std::filesystem::directory_iterator boostIsShit(path);
         for (std::filesystem::directory_entry entry : boostIsShit) try {
-            const std::string entryName = entry.path().filename();
+            const std::string entryName = entry.path().filename().string();
             if ((entryName == "Data" || entryName == "resources") && entry.is_directory()) {
                 hasDataFolder = true;
             }
