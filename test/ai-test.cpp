eae021311b78271476646fec2313710a30da7884
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-12-24 16:08:36 +0100

    add simple test application for ai script parsing

---
+++ test/ai-test.cpp
@@ -0,0 +1,17 @@
+#include "ai/ScriptLoader.h"
+
+#include <fstream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "starting" << std::endl;
+       if (argc < 2) {
+           std::cerr << "pass file" << std::endl;
+           return 1;
+       }
+       std::ifstream in;
+       in.open(argv[1]);
+       std::cout << in.good() << std::endl;
+       ai::ScriptLoader parser(0);
+       return parser.parse(in, std::cout);
+}
+
