dbae703d76b1fc46dc16fa5083e300d597f89303
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-10-17 19:28:37 +0200

    try to fix macos

---
+++ src/audio/Implementation_macos.mm
@@ -0,0 +1,7 @@
+#define MINIAUDIO_IMPLEMENTATION
+#define MA_NO_JACK
+#define MA_NO_SDL
+#define MA_NO_OPENAL
+
+#include <miniaudio/miniaudio.h>
+
--- src/audio/Implementations.cpp
+++ src/audio/Implementations.cpp
@@ -1,6 +1,7 @@
 #define DR_MP3_IMPLEMENTATION
 #include <miniaudio/extras/dr_mp3.h>
 
+#ifndef __APPLE__
 #define MINIAUDIO_IMPLEMENTATION
 #define MA_NO_JACK
 #define MA_NO_SDL
@@ -8,3 +9,4 @@
 #define MA_NO_ALSA
 //#define ma_DEBUG_OUTPUT
 #include <miniaudio/miniaudio.h>
+#endif//__APPLE__
