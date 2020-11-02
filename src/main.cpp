b562b74cfba1fcd0fce771ce37dc2bfc2a10bfec
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-11-02 18:16:38 +0100

    mostly just to force a CI rebuild

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2011  <copyright holder> <email>
+    Main entry point.
+    Copyright (C) 2017-2020 Martin Sandsmark <martin.sandsmark@kde.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -194,7 +194,6 @@ bool requestFilePath(const std::string &errorMessage)
     return true;
 }
 
-// TODO: Bad_alloc
 int main(int argc, char **argv)
 #ifdef NDEBUG
 try
